c354a1813ef23971bc0cdec29fa74b890d6a7095
Luciano Ponce <poncestigaardluciano@gmail.com>
2025-08-17 22:19:16 -0300

    se creo el componente Card y se estilo

--- MorfinaApp/src/Components/Cards.jsx
+++ MorfinaApp/src/Components/Cards.jsx
@@ -3,14 +3,18 @@ import "./Styles/cards.css";
 const Cards = () => {
   return (
     <div className="cardsContainer">
+      {" "}
+      {/*contenedor gral*/}
       <div className="imagenesPortada">
+        {/*SUB-contenedor gral*/}
         <div className="imgCard">
-          <h4 className="tituloCard">Voz (Lider)</h4>
+          {/*contenedor de card particular */}
+          <h4 className="tituloCard">Voz</h4>
           <img src=".\public\img\martin1.jpg" alt="imagen Martin" />
         </div>
 
         <div className="imgCard">
-          <h4 className="tituloCard">Bateria (percusion)</h4>
+          <h4 className="tituloCard">Bateria</h4>
           <img src=".\public\img\ariel1.jpg" alt="Imagen Ariel" />
         </div>
 
@@ -20,17 +24,17 @@ const Cards = () => {
         </div>
 
         <div className="imgCard">
-          <h4 className="tituloCard">Guitarra (1° y 2°)</h4>
+          <h4 className="tituloCard">Guitarra</h4>
           <img src=".\public\img\agus1.jpg" alt="Imagen Agustin" />
         </div>
 
         <div className="imgCard">
-          <h4 className="tituloCard">Guitarra (1° y 2°)</h4>
+          <h4 className="tituloCard">Guitarra </h4>
           <img src=".\public\img\braianM1.jpg" alt="Imagen Braian M" />
         </div>
 
         <div className="imgCard">
-          <h4 className="tituloCard">Teclados y Coros</h4>
+          <h4 className="tituloCard">Teclados</h4>
           <img src=".\public\img\luciano1.jpg" alt="Imagen Luciano" />
         </div>
       </div>
